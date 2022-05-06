088b170bd5b2a82c309a2fdc08898c27e6af2a63
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-05-06 19:42:38 +0100

    Primeiro commit - projeto criado

---
+++ src/main/java/com/marianarosa/Aula61aula62sbmongodb/Aula62SbMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.marianarosa.Aula61aula62sbmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula62SbMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula62SbMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marianarosa/Aula61aula62sbmongodb/Aula62SbMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marianarosa.Aula61aula62sbmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula62SbMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
