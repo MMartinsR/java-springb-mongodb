5b0d19c243da43634fe3d026c34f1e98c47f92fa
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-05-09 18:18:14 +0100

    Insercao de usuario com POST

--- src/main/java/com/marianarosa/Aula61aula62sbmongodb/services/UserService.java
+++ src/main/java/com/marianarosa/Aula61aula62sbmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marianarosa.Aula61aula62sbmongodb.domain.User;
+import com.marianarosa.Aula61aula62sbmongodb.dto.UserDTO;
 import com.marianarosa.Aula61aula62sbmongodb.repository.UserRepository;
 import com.marianarosa.Aula61aula62sbmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id){
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
