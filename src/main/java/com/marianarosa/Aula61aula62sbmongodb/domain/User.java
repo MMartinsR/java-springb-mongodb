c852d44822c27459284693aaf569652dccd0ee2e
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-05-10 18:22:33 +0100

    Referenciando os posts do usuário

--- src/main/java/com/marianarosa/Aula61aula62sbmongodb/domain/User.java
+++ src/main/java/com/marianarosa/Aula61aula62sbmongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.marianarosa.Aula61aula62sbmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -15,11 +18,15 @@ public class User implements Serializable{
 	private String name;
 	private String email;
 	
+	// lazy = true - garante que numa consulta a usuários, os posts apenas serão carregados
+	// se forem explicitamente acessados
+	@DBRef(lazy = true)  
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
 	}
 
 	public User(String id, String name, String email) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -47,6 +54,14 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
