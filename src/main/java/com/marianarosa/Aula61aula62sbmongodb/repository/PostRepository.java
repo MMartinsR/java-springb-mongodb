4ef2c33893d8491e688ba9d96b169ceefba1b7d7
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-05-12 00:47:51 +0100

    Consulta simples com query methods

--- src/main/java/com/marianarosa/Aula61aula62sbmongodb/repository/PostRepository.java
+++ src/main/java/com/marianarosa/Aula61aula62sbmongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.marianarosa.Aula61aula62sbmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import com.marianarosa.Aula61aula62sbmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	
+	// IgnoreCase, faz o método ignorar a diferença entre minusculas e maiusculas
+	List<Post> findByTitleContainingIgnoreCase(String text);  
 
 }
