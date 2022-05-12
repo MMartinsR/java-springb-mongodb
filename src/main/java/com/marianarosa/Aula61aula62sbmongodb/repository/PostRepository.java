d6ed1ba98249f8b131c65110e2727e1fc8d62f04
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-05-12 02:28:30 +0100

    Consulta com vários critérios

--- src/main/java/com/marianarosa/Aula61aula62sbmongodb/repository/PostRepository.java
+++ src/main/java/com/marianarosa/Aula61aula62sbmongodb/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.marianarosa.Aula61aula62sbmongodb.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -17,6 +18,13 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	
 	// Query methods
 	// IgnoreCase, faz o método ignorar a diferença entre minusculas e maiusculas
-	List<Post> findByTitleContainingIgnoreCase(String text);  
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	// Pesquisa com vários critérios]}
+	@Query("{ $and: [{date: {$gte: ?1}},{date: {$lte: ?2}},"
+			+ "{ $or: [{ 'title': {$regex: ?0, $options: 'i'} }, "
+			+ "{ 'body': { $regex: ?0, $options: 'i' } }, "
+			+ "{ 'comments.text': { $regex: ?0, $options: 'i' } }]}] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
